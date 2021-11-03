34e4aec1dca69ddb539f0852e812db38f71a953b
Frosty <bluedogdk@gmail.com>
2021-11-03 22:13:13 +0100

    CreatePiece added

---
+++ Assets/CreatePieces.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePieces : MonoBehaviour
+{
+
+    private Vector3 defaultSize = new Vector3(2.1f, 2.1f, 0.1f);
+    private float defaultX = -9.844f;
+    private float defaultY = 7.719f;
+    private float defaultZ = -1.29f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
