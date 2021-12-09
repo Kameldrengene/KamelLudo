911f5547e570bf60306adf50d906dc58123b3ad9
Frosty <bluedogdk@gmail.com>
2021-12-09 15:44:39 +0100

    Location list for start pos for pieces

--- Assets/UpdateBoard.cs
+++ Assets/UpdateBoard.cs
@@ -9,6 +9,21 @@ public class UpdateBoard : MonoBehaviour
 
     private List<int> piecesAlive = new List<int> { 4, 4, 4, 4 };
     public List<Piece> pieces = Board.pieces;
+    public List<Vector3> pVector = new List<Vector3>();
+
+    private List<Vector3> getAllPos()
+    {
+        if(pVector.Count > 0)
+        {
+            return pVector;
+        }
+        List<Vector3> tempList = new List<Vector3>();
+        foreach(Piece p in pieces)
+        {
+            tempList.Add(p.pieceObject.transform.position);
+        }
+        return tempList;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -35,19 +50,17 @@ public class UpdateBoard : MonoBehaviour
     public void onBlueClick()
     {
         //UpdateDeadPieces();
+        pVector = getAllPos();
         UpdatePiecesPosition();
     }
 
     private int tick = 0;
-    private Vector3 temp;
     private void UpdatePiecesPosition()
     {
         if (tick == 0)
         {
             pieces[0].field = CreateFields.Instance.getStart(pieces[0].getPieceColor());
             Vector3 pos = pieces[0].field.field.transform.position;
-            Vector3 pos2 = pieces[0].pieceObject.transform.position;
-            temp = new Vector3(pos2[0], pos2[1], pos2[2]);
             pieces[0].pieceObject.transform.position = new Vector3(pos[0], pos[1], pieces[0].pieceObject.transform.position[2]);
             tick++;
             Debug.Log("pos: " + pos);
@@ -55,10 +68,10 @@ public class UpdateBoard : MonoBehaviour
         else
         {
             Debug.Log("Piece location updated");
-            pieces[0].pieceObject.transform.position = temp;
+            pieces[0].pieceObject.transform.position = pVector[0];
             tick = 0;
         }
-        Debug.Log("pos T: " + temp);
+        Debug.Log("pos T: " + pVector[0]);
 
     }
 
